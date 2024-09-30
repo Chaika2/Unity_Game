038a84a9c0f26aa47ab1246f58b0c6f18d067122
Chaik <Chaika1448@yandex.ru>
2024-09-30 13:39:43 +0500

    Слежка камеры
    Камера теперь следует за игроком

--- игрулька/Assets/Scripts/GameInput.cs
+++ игрулька/Assets/Scripts/GameInput.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
@@ -20,5 +21,10 @@ public class GameInput : MonoBehaviour
     public Vector2 GetMovementVector(){
         Vector2 inputVector = playerInputAcrions.Player.Move.ReadValue<Vector2>();
         return inputVector;
+    }
+
+    public Vector3 GetMousePosition(){
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
     }
 }
