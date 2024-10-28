d11a2976c7900f6db8c6eeb18860679ae836b2af
Voyage <kirillbatalovwork@mail.ru>
2024-10-28 16:49:25 +0500

    Menu sound changes

--- Universe Crusader/Assets/Scripts/MainMenu.cs
+++ Universe Crusader/Assets/Scripts/MainMenu.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenu : Sounds
 {
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
    public void PlayGame()
    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        PlaySound(sounds[0]);
+        Invoke("LoadScene", 1);
    }
 
 
 
    public void ExitGame()
    {
+        PlaySound(sounds[0]);
+        Invoke("Quit", 1);
         Debug.Log("Игра закрылась");
-        Application.Quit();
+
    }
 
 }
+
