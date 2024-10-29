86069c06b5c56e29356febf0d0ee357383136817
Voyage <kirillbatalovwork@mail.ru>
2024-10-30 01:44:25 +0500

    New Assets + Menu update

--- Universe Crusader/Assets/Scripts/Danger.cs
+++ Universe Crusader/Assets/Scripts/Danger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Danger : MonoBehaviour
+public class Danger : Sounds
 {
     // Start is called before the first frame update
     Rigidbody2D rb;
