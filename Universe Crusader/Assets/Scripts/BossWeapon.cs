3e9ff05d2d575c792afbcab0bc0f0506a7120647
Voyage <kirillbatalovwork@mail.ru>
2024-11-04 20:53:07 +0500

    Boss

---
+++ Universe Crusader/Assets/Scripts/BossWeapon.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BossAttack : MonoBehaviour
+{
+    public float AttackDamage = 20;
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
+    private Animator anim;
+    Transform player;
+        public void OnEnemyAttack()
+        {
+            Vector3 pos = transform.position;
+            pos += transform.right * attackOffset.x;
+            pos += transform.up * attackOffset.y;
+
+            Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+   
+            if (colInfo != null)
+            {
+                colInfo.GetComponent<Player>().TakeDamage(AttackDamage);
+            }
+        }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Gizmos.DrawWireSphere(pos, attackRange);
+    }
+}
+    
+  
---
+++ Universe Crusader/Assets/Scripts/HpBar.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
+{
+
+    private float HP = 100f;
+    public Image Bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollsionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            HP -= 5;
+            Bar.fillAmount = HP/100;
+        }
+    }
+}
